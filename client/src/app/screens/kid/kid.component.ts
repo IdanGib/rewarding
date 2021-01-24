83c466e018eac6e274326c330f7b7bae40f7cf7e
IdanGib <idangibly.dev@gmail.com>
2021-01-24 21:00:04 +0200

    stars

--- client/src/app/screens/kid/kid.component.ts
+++ client/src/app/screens/kid/kid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+import { KidProfile } from 'src/app/logic/interfaces';
 
 @Component({
   selector: 'app-kid',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kid.component.scss']
 })
 export class KidComponent implements OnInit {
-  title = 'Kid';
-  constructor() { }
-
+  kid: KidProfile;
+  constructor(private route: ActivatedRoute, private app: AppService) { }
+  rate(r: number) {
+    this.kid.stars += r;
+    this.app.saveState();
+  }
   ngOnInit(): void {
+    const display = this.route.snapshot.params['id'];
+    this.kid = this.app.state.kids.find(k => k.display === display);
   }
 
 }
